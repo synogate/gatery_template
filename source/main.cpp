f8daf12317554e01e04b2db97268190c61bd8ad8
Andy <32514680+Andreas-Ley@users.noreply.github.com>
2022-11-26 14:09:14 +0000

    Various tweaks and fixes

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,8 @@
 #include <gatery/simulation/waveformFormats/VCDSink.h>
 #include <gatery/simulation/ReferenceSimulator.h>
 
+#include <iostream>
+
 using namespace gtry;
 using namespace gtry::vhdl;
 using namespace gtry::scl;
@@ -25,11 +27,11 @@ int main()
 	}
 
 	// Build circuit
-	Clock clock{1'000}; // 1KHz
+	Clock clock{{.absoluteFrequency = 1'000}}; // 1KHz
 	ClockScope clockScope{ clock };
 
 	hlim::ClockRational blinkFrequency{1, 1}; // 1Hz
-	size_t counterMax = hlim::floor(clock.getAbsoluteFrequency() / blinkFrequency);
+	size_t counterMax = hlim::floor(clock.absoluteFrequency() / blinkFrequency);
 	UInt counter = BitWidth(utils::Log2C(counterMax+1));
 	auto enable = pinIn().setName("button");
 	
@@ -39,7 +41,8 @@ int main()
 	counter = reg(counter, 0);
 	HCL_NAMED(counter);
 
-	pinOut(counter.msb()).setName("led");
+	Bit ledOn = counter.msb();
+	pinOut(ledOn).setName("led");
 
 	design.postprocess();
 
@@ -48,10 +51,27 @@ int main()
 	simulator.compileProgram(design.getCircuit());
 
 	simulator.addSimulationProcess([=, &clock]()->SimProcess{
+
+		fork([=, &clock]()->SimProcess{
+			while (true) {
+				co_await OnClk(clock);
+
+				if (simu(ledOn) == '1')
+					std::cout << "LED is on" << std::endl;
+				else if (simu(ledOn) == '0')
+					std::cout << "LED is off" << std::endl;
+				else
+					std::cout << "LED is undefined" << std::endl;
+			}
+		});
+
+
+		std::cout << "Disabling" << std::endl;
 		simu(enable) = '0';
-		for ([[maybe_unused]]auto i : Range(300))
+		for ([[maybe_unused]]auto i : Range(50))
 			co_await AfterClk(clock);
 
+		std::cout << "Enabling" << std::endl;
 		simu(enable) = '1';
 	});
 
