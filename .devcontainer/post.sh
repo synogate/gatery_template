d90490e9354d5a60e8d059f0afe70d26d0ff3490
Andreas Ley <andreas.ley@synogate.com>
2024-10-18 00:05:22 +0200

    Added missing boost-flyweight to devcontainer.

--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+# todo: I need to update the docker image
+vcpkg install boost-flyweight
+
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE:STRING=/usr/local/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE:STRING=/usr/local/vcpkg/scripts/buildsystems/vcpkg.cmake -DBoost_NO_WARN_NEW_VERSIONS=1
 make -j 2
